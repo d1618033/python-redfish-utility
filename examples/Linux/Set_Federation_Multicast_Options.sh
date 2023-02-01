cf0c002f204656a3188e04758f47bc0824759049
Rajeev Kallur <kallur2014@gmail.com>
2023-02-01 20:32:49 +0530

    Update Set_Federation_Multicast_Options.sh

--- examples/Linux/Set_Federation_Multicast_Options.sh
+++ examples/Linux/Set_Federation_Multicast_Options.sh
@@ -19,7 +19,7 @@
 
 runLocal(){
   ilorest select Manager. -u USER_LOGIN -p PASSWORD
-  ilorest set Oem/Hpe/FederationConfig/iLOFederationManagerment=Enabled
+  ilorest set Oem/Hpe/FederationConfig/iLOFederationManagement=Enabled
   # Notes:                                                          #
   # Disabling multicast discovery or announcements will             #
   #    disable iLO Federation features.                             #
@@ -27,7 +27,7 @@ runLocal(){
   #    All devices in a Federation group should have the same       #
   #    scope and TTL to avoid problems with peer discovery.         #
 
-  ilorest set Oem/Hpe/FederationConfig/MulticastDiscoveryEnabled=Enabled
+  ilorest set Oem/Hpe/FederationConfig/MulticastDiscovery=Enabled
   # Valid values for MulticastAnnouncementInterval are              #
   #    "Disabled", "30", "60", "120", "300", "600",                 #
   #    "900", and "1800".  Numeric values are in seconds.           #
